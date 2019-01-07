314f739cf9e5520a09a6034bba3b0f5daee62537
Haoze Yu <1103255467@qq.com>
2019-01-07 21:34:55 +0800

    3 添加Adress相关文件

---
+++ src/main/java/com/ssy/app/enity/Adress.java
@@ -0,0 +1,53 @@
+package com.ssy.app.enity;
+
+public class Adress {
+    private Long id;
+
+    private Long uid;
+
+    private String adress;
+
+    private String phone;
+
+    private Integer status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress == null ? null : adress.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
