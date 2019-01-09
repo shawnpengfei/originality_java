c7db70a2aa8c22432c68c341e3228aaf8491b517
szw <2801008944@qq.com>
2019-01-09 18:10:13 +0800

    添加图片分页展示接口 添加图片分类、分页展示接口

--- src/main/java/com/ssy/app/service/SeeImgService.java
+++ src/main/java/com/ssy/app/service/SeeImgService.java
@@ -3,6 +3,7 @@ package com.ssy.app.service;
 
 import com.ssy.app.enity.SeeImg;
 import com.ssy.app.vo.JsonBean;
+import com.ssy.app.vo.PageBeanVo;
 
 public interface SeeImgService {
 
@@ -12,11 +13,16 @@ public interface SeeImgService {
 
     JsonBean addSeeImgSelective(SeeImg record);
 
+    //根据查看图片
     JsonBean selectSeeImgById(Long id);
 
     JsonBean updateByIdSelective(SeeImg record);
 
     JsonBean updateById(SeeImg record);
 
+    PageBeanVo showImgAll(Integer page, Integer count);
 
+    PageBeanVo showSeeImgByClass(Integer cid, Integer page, Integer count);
+
+    JsonBean starImg(Integer star);
 }
