6cf5a62e88164099304cd57814798027593ebdcd
Haoze Yu <1103255467@qq.com>
2019-01-14 14:00:22 +0800

    添加了拦截器的跨域设置

--- src/main/java/com/ssy/app/interceptor/TokenInterceptor.java
+++ src/main/java/com/ssy/app/interceptor/TokenInterceptor.java
@@ -1,17 +1,24 @@
 package com.ssy.app.interceptor;
 
 import com.ssy.app.utils.YhzUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class TokenInterceptor implements HandlerInterceptor {
 
 
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        httpServletResponse.addHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.addHeader("Access-Control-Allow-Headers", "Authentication,Origin, X-Requested-With, Content-Type, Accept,token");
+
         String token = httpServletRequest.getHeader("token");
         if(token != null){
             boolean rr = YhzUtils.verify(token);
